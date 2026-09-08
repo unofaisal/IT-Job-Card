122b542deed158071d6607136623104b275383ba
unofaisal <faisalimali1@gmail.com>
2026-09-08 15:26:52 +0300

    feat: Update logo path for IT Job Card in apps screen configuration

--- it_job_card/hooks.py
+++ it_job_card/hooks.py
@@ -260,7 +260,7 @@ add_to_apps_screen = [
     {
         "name": "it_job_card",
         "title": "IT Job Card",
-        # "logo": "/assets/it_job_card/images/logo.png",  # optional
+        "logo": "/assets/it_job_card/images/logo.png",  # optional
         "route": "/app/it-job-card",
     }
 ]
